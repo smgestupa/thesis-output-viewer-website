7af1c2219aefc00b4c092c35ce9b30d24dd6a656
smgestupa <thereal.nwahs@gmail.com>
2024-01-16 20:49:02 +0800

    remove border class

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 
 <div class="flex flex-grow gap-x-5 px-[3.125rem] pt-[3.125rem]">
 	<Nav />
-	<div class="flex flex-col w-full border">
+	<div class="flex flex-col w-full">
 		<slot />
 	</div>
 </div>
